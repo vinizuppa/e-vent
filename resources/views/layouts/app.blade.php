385c8716a55e709078745f18b12f3c6ce8e7e93a
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2021-03-02 17:25:30 -0300

    Adicionado favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
         <title>{{ config('app.name', 'E-vent') }}</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" />
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
