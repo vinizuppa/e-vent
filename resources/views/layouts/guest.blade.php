67675fdf1a12b30d370525fad95397479f917d07
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2021-03-09 21:04:58 -0300

    Removidos arquivos não utilizados

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,7 @@
         <title>{{ config('app.name', 'E-vent') }}</title>
         <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="{{ asset('css/app.css') }}" type="stylesheet" />
     </head>
     <body>
         <x-navbar />
@@ -14,5 +15,6 @@
             {{ $slot }}
         </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
