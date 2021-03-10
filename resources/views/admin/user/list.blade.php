3e2e76c82cddf5a167b62e7644f0443f5fc6fba0
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2021-03-09 21:36:27 -0300

    Atualizado lista de usuários, criada tabela para exibir dados

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -1,6 +1,35 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h1>Lista de usuários</h1>
-    </x-slot>
-    {{ $users }}
+    <div class="card">
+        <div class="card-header">
+            <h1>Usuários</h1>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Criado</th>
+                    <th scope="col">Atualizado</th>
+                    <th scope="col">Ações</th>
+                </thead>
+                <tbody>
+                    @forelse ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ date('d/m/Y H:i:s', strtotime($user->created_at)) }}</td>
+                            <td>{{ date('d/m/Y H:i:s', strtotime($user->updated_at)) }}</td>
+                            <td>Info / Editar / Excluir</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="6">Sem dados</td>
+                        <tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
 </x-app-layout>
