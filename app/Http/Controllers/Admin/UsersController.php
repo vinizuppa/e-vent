b4139faf5d6b34369ea7cfdb9e26616394b183ff
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2021-03-20 14:35:51 -0300

    Ajustes validations

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -53,6 +53,13 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+            'username' => 'nullable|string|max:255|unique:users'
+        ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
