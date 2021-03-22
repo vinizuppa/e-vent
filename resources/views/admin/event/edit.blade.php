e74af6c816c60e0f451aec14499532c91d307dee
Vinicus Cordeiro Zuppa <vcordeiro12@gmail.com>
2021-03-22 20:54:11 -0300

    Adicionada função em Js para formatar campo de valor.

--- resources/views/admin/event/edit.blade.php
+++ resources/views/admin/event/edit.blade.php
@@ -33,7 +33,7 @@
 
         <div class="col-md-6">
             <label for="registration_fee" class="form-label">Valor Inscrição R$</label>
-            <input id="registration_fee" type="text" class="form-control @error('registration_fee') is-invalid @enderror" name="registration_fee" registration_fee="{{ old('registration_fee') }}" autocomplete="registration_fee">
+        <input id="registration_fee" type="text" class="form-control @error('registration_fee') is-invalid @enderror" name="registration_fee" registration_fee="{{ old('registration_fee') }}" autocomplete="registration_fee" onkeyup="formatarMoeda(this);">
             @error('registration_fee')
                 <div class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,15 @@
                     mascara( this, mtel );
                 }
             }
+
+            function formatarMoeda(i){
+                var v = i.value.replace(/\D/g,'');
+	            v = (v/100).toFixed(2) + '';
+	            v = v.replace(".", ".");
+	            v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3.");
+	            v = v.replace(/(\d)(\d{3}),/g, "$1.$2.");
+	            i.value = v;
+            }
         </script>
 
     </head>
