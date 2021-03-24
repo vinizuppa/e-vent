6178aa446b5b6ca40c05dd232da29f11c13166ec
Vinicus Cordeiro Zuppa <vcordeiro12@gmail.com>
2021-03-23 23:09:45 -0300

    Adicionado icones e Tooltips no index.blade.php de events.

--- resources/views/admin/event/index.blade.php
+++ resources/views/admin/event/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="title">Eventos</x-slot>
-    <a href="{{ route('events.create') }}" class="btn btn-primary mb-2">Novo</a>
+    <a href="{{ route('events.create') }}" class="btn btn-primary mb-2"><i class="bi bi-plus-circle"></i> Novo</a>
     <div class="table-responsive">
         <table class="table table-striped table-bordered">
             <thead class="table-light">
@@ -18,14 +18,22 @@
                         <td>{{ $event->startDate() }} - {{ $event->endDate() }} ({{ $event->eventDuration() }})</td>
                         <td>{{ ($event->registration_fee == 0) ? 'Sem taxa' : 'R$ ' . $event->registration_fee }}</td>
                         <td>
-                            <a href="{{ route('events.show', $event->id) }}" class="btn btn-primary">
-                                Info
+                            <a href="{{ route('events.show', $event->id) }}" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="Atividades">
+                                <i class="bi bi-card-checklist"></i>
+                                
                             </a>
-                            <a href="{{ route('events.edit', $event->id) }}" class="btn btn-warning">
-                                Editar
+
+                            <a href="{{ route('events.show', $event->id) }}" class="btn btn-primary" data-toggle="tooltip" title="Info">
+                                <i class="bi bi-info-circle"></i>
+                                
                             </a>
-                            <a href="#" id="btnExcluir" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalExcluir" data-event="{{ $event }}" data-route="{{ route('events.destroy', $event->id) }}">
-                                Excluir
+                            <a href="{{ route('events.edit', $event->id) }}" class="btn btn-warning" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar">
+                                <i class="bi bi-pencil"></i>
+                                
+                            </a>
+                            <a href="#" id="btnExcluir" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalExcluir" data-event="{{ $event }}" data-route="{{ route('events.destroy', $event->id) }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Excluir">
+                                <i class="bi bi-trash"></i>
+                                
                             </a>
                             <div class="modal fade" id="modalExcluir" tabindex="-1" aria-labelledby="modalExcluir" aria-hidden="true">
                                 <div class="modal-dialog">
@@ -33,6 +41,7 @@
                                         <div class="modal-header">
                                             <h5 class="modal-title">Excluir Evento</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            
                                         </div>
                                         <div class="modal-body">
                                             Deseja excluir o Evento?
@@ -44,6 +53,7 @@
                                                 <button type="submit" class="btn btn-danger">Excluir</button>
                                             </form>
                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                            
                                         </div>
                                     </div>
                                 </div>
