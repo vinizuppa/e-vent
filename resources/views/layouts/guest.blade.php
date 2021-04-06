f4f2cac26c77e83e4affbaebdbad8de8a6f6ca7e
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2021-04-06 20:57:51 -0300

    Adicionado AdSense ao site, somente em páginas públicas

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,4 +3,7 @@
     <main class="container-fluid mt-3">
         {{ $slot }}
     </main>
+    <x-slot name="js">
+        <script data-ad-client="ca-pub-8771460495164377" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    </x-slot>
 </x-base-layout>
