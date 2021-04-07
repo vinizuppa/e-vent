6bfd9dc04620170a06ad9754c9a5979668eca3c1
Danilo Perez <daniloperez2016@outlook.com>
2021-04-06 23:21:11 -0300

    Atualização do layout da view datail.blade, detalhes do evento

--- resources/views/public/events/detail.blade.php
+++ resources/views/public/events/detail.blade.php
@@ -1,46 +1,50 @@
 <x-guest-layout>
-    <x-slot name="title">Informações do Evento</x-slot>
-        <div class="mb-3 row">
-            <label for="name" class="col-sm-1 col-form-label">Nome: </label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control-plaintext-plaintext" id="name" value="{{ $event->name }}" disabled>
-            </div>
-        </div>        
-        <div class="mb-3 row">
-            <label for="description" class="col-sm-1 col-form-label">Descrição: </label>
-            <div class="col-sm-3">
-                <textarea class="form-control-plaintext" id="description" disabled>{{ $event->description }}</textarea>
-            </div>
-        </div>
-        <div class="mb-3 row">
-            <label for="address" class="col-sm-1 col-form-label">Endereço: </label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control-plaintext" id="address" value="{{ $event->address }}" disabled>
-            </div>
-        </div>
-        <div class="mb-3 row">
-            <label for="phone" class="col-sm-1 col-form-label">Telefone: </label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control-plaintext" id="phone" value="{{ $event->phone }}" disabled>
+    <div class="container">
+        <div class="card shadow bg-dark rounded-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="http://centralbme.com.br/blog/wp-content/uploads/Como-um-evento-corporativo-pode-ajudar-a-sua-empresa.jpg" alt="Banner do evento" class="w-100 h-100 img-fluid">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title mx-2 text-white">Informações do Evento</h2>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-bookmark-star text-danger"></i>{{ $event->name }}</h4>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-file-earmark-text text-danger"></i>{{ $event->description }}</h4>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-geo-alt text-danger"></i>{{ $event->address }}</h4>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-telephone text-danger"></i>{{ $event->phone }}</h4>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-calendar2-check text-danger"></i>{{ $event->startDate() }}</h4>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-calendar2-x text-danger"></i>{{ $event->endDate() }}</h4>
+                        <h4 class="card-text my-3 text-white"><i class="mx-2 bi bi-cash-stack text-danger"></i>{{ $event->registration_fee }}</h4>
+                        <div class="d-flex justify-content-end">
+                            <a href="#" class="btn btn-danger">Inscreva-se</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="mb-3 row">
-            <label for="registration_fee" class="col-sm-1 col-form-label">Valor: </label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control-plaintext" id="registration_fee" value="{{ $event->registration_fee }}" disabled>
-            </div>
+        <div class="table-responsive mx-auto mt-3 rounded-3">
+            <table class="table table-dark table-striped">
+                <thead>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Instruções</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Vagas disponíveis</th>
+                </thead>
+                <tbody>
+                    @forelse ($event->activities as $activity)
+                        <tr>
+                            <td>{{ $activity->name }}</td>
+                            <td>{{ $activity->instructions }}</td>
+                            <td>{{ $activity->type }}</td>
+                            <td>{{ $activity->vacancies }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="6">Sem dados</td>
+                        <tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
-        <div class="mb-3 row">
-            <label for="start_date" class="col-sm-1 col-form-label">Início: </label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control-plaintext" id="start_date" value="{{ $event->start_date }}" disabled>
-            </div>
-        </div>
-        <div class="mb-3 row">
-            <label for="end_date" class="col-sm-1 col-form-label">Fim: </label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control-plaintext" id="end_date" value="{{ $event->end_date }}" disabled>
-            </div>
-        </div>
-    </form>
+    </div>    
 </x-guest-layout>
